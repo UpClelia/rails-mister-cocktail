75a2fed7dc554d423eabba2a019d97a224e04ccb
Clelia Sapriel <clelia_sapriel@yahoo.fr>
2018-11-29 19:33:01 +0000

    mrcocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :cocktails, only: [:new, :create, :index, :show] do
+    resources :dose, only: [:new, :create]
+  end
 end
